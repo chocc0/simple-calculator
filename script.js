6d94bef4b33bce42e8630ded349ed27debfd836f
Mandy <mandysun453@gmail.com>
2022-08-13 16:23:17 -0500

    add event listeners for operators, numbers, and for basic calculations

--- script.js
+++ script.js
@@ -8,23 +8,56 @@ const raisePowerToNum = (a,b) => (a**b);
 
 //operate function
 function operate(operation, a, b) {
+    let numA = parseInt(a);
+    let numB = parseInt(b)
     if (operation === '+') {
-        return addNum(a,b)
+        return addNum(numA, numB)
     } else if (operation === '-') {
-        return subtractNum(a,b)
-    } else if (operation === '*') {
-        return multNum(a,b)
+        return subtractNum(numA, numB)
+    } else if (operation === 'x') {
+        return multNum(numA, numB)
     } else if (operation === '/') {
-        return divNum(a,b)
+        return divNum(numA, numB)
     } else if (operation === '^') {
-        return raisePowerToNum(a,b)
+        return raisePowerToNum(numA, numB)
     }
 }
 
-//testing vvv
+//display numbers
+let display = document.getElementById("display");
 
+//display number buttons
+let numBtns = document.querySelectorAll("#number-container button");
+
+numBtns.forEach(button => {
+    button.addEventListener('click', (e) => {
+        display.textContent += (button.textContent)
+    })
+})
+
+//display operator buttons
+let operatorBtns = document.querySelectorAll("#operator-container #operators button");
+let calculate = document.querySelector("#calculate")
+
+operatorBtns.forEach(button => {
+    button.addEventListener('click', () => {
+        display.textContent += (' ' + button.textContent + ' ')
+    })
+})
+
+calculate.addEventListener('click', () => {
+    let contentArray = display.textContent.split(" ");
+    let result = operate(contentArray[1], contentArray[0], contentArray[2])
+    display.textContent = result
+})
+
+
+
+
+//testing vvv
+/*
 let num1 = parseInt(prompt())
 let num2 = parseInt(prompt())
 let choice = prompt()
 
-console.log(operate(choice, num1, num2))
+console.log(operate(choice, num1, num2))*/
